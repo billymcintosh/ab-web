9797c48695d51dc0cc97eb20840cf7fcf61b1576
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-09-21 19:52:06 +0700

    show loading msg if prop isn't ready

--- app/components/Dashboard/Economy.js
+++ app/components/Dashboard/Economy.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
-import List from '../List';
 
 import { formatAbp, formatNtz, percent3FixedDec } from '../../utils/amountFormatter';
+
+import WithLoading from '../WithLoading';
+import List from '../List';
 
 const Economy = (props) => {
   const {
@@ -18,18 +20,54 @@ const Economy = (props) => {
   const ECONOMY_LIST = [
     [
       <FormattedMessage {...messages.economyListOwnership} />,
-      <FormattedMessage values={{ amount: percent3FixedDec(babzBalance / totalSupplyNTZ) }} {...messages.percentUnit} />,
-      <FormattedMessage values={{ amount: percent3FixedDec(pwrBalance / totalSupplyABP) }} {...messages.percentUnit} />,
+      <WithLoading
+        isLoading={!babzBalance && !totalSupplyNTZ}
+        loadingSize="14px"
+        type="inline"
+      >
+        <FormattedMessage values={{ amount: percent3FixedDec(babzBalance / totalSupplyNTZ) }} {...messages.percentUnit} />
+      </WithLoading>,
+      <WithLoading
+        isLoading={!pwrBalance && !totalSupplyABP}
+        loadingSize="14px"
+        type="inline"
+      >
+        <FormattedMessage values={{ amount: percent3FixedDec(pwrBalance / totalSupplyABP) }} {...messages.percentUnit} />
+      </WithLoading>,
     ],
     [
       <FormattedMessage {...messages.economyListActive} />,
-      <FormattedMessage values={{ amount: formatNtz(totalSupplyNTZ) }} {...messages.ntzUnit} />,
-      <FormattedMessage values={{ amount: formatAbp(activeSupplyABP) }} {...messages.abpUnit} />,
+      <WithLoading
+        isLoading={!totalSupplyNTZ}
+        loadingSize="14px"
+        type="inline"
+      >
+        <FormattedMessage values={{ amount: formatNtz(totalSupplyNTZ) }} {...messages.ntzUnit} />
+      </WithLoading>,
+      <WithLoading
+        isLoading={!activeSupplyABP}
+        loadingSize="14px"
+        type="inline"
+      >
+        <FormattedMessage values={{ amount: formatAbp(activeSupplyABP) }} {...messages.abpUnit} />
+      </WithLoading>,
     ],
     [
       <FormattedMessage {...messages.economyListTotal} />,
-      <FormattedMessage values={{ amount: formatNtz(totalSupplyNTZ) }} {...messages.ntzUnit} />,
-      <FormattedMessage values={{ amount: formatAbp(totalSupplyABP) }} {...messages.abpUnit} />,
+      <WithLoading
+        isLoading={!totalSupplyNTZ}
+        loadingSize="14px"
+        type="inline"
+      >
+        <FormattedMessage values={{ amount: formatNtz(totalSupplyNTZ) }} {...messages.ntzUnit} />
+      </WithLoading>,
+      <WithLoading
+        isLoading={!totalSupplyABP}
+        loadingSize="14px"
+        type="inline"
+      >
+        <FormattedMessage values={{ amount: formatAbp(totalSupplyABP) }} {...messages.abpUnit} />
+      </WithLoading>,
     ],
   ];
   const COL_STYLE = {
